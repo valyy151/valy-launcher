eb24b6c9fa660eba905df693ba4e44ac5538b377
valyy151 <marin.valenta1@gmail.com>
2023-05-05 09:29:42 +0200

    Enviroment variables project name question

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,21 @@
-// #!/usr/bin/env node
+#! /usr/bin/env node
+
+const RESET = '\x1b[0m'
+const RED = '\x1b[31m'
+const GREEN = '\x1b[32m'
+const YELLOW = '\x1b[33m'
+const BLUE = '\x1b[34m'
+const MAGENTA = '\x1b[35m'
+const CYAN = '\x1b[36m'
+const ORANGE = '\x1b[38;2;255;165;0m'
+
 const { execSync } = require('child_process')
+const readline = require('readline')
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+})
 
 const runCommand = (command) => {
 	try {
@@ -11,25 +27,39 @@ const runCommand = (command) => {
 	return true
 }
 
-const repoName = process.argv[2]
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/valyy151/valy-launcher ${repoName}`
-const installDepsCommand = `cd ${repoName} && npm install`
-
-console.log('Cloning into ${repoName}...')
-
-const checkedOutput = runCommand(gitCheckoutCommand)
-
-if (!checkedOutput) {
-	console.error('Failed to clone into repo')
-	process.exit(1)
+let repoName = process.argv[2]
+if (!repoName) {
+	console.clear()
+	rl.question(`${CYAN}Enter the name of your project:${RESET}`, (name) => {
+		repoName = name.trim()
+		cloneAndInstall(repoName)
+		rl.close()
+	})
+} else {
+	cloneAndInstall(repoName)
 }
 
-console.log('Installing dependencies...')
+function cloneAndInstall(repoName) {
+	const gitCheckoutCommand = `git clone --depth 1 https://github.com/valyy151/valy-launcher ${repoName}`
+	const installDepsCommand = `cd ${repoName} && npm install`
+	console.clear()
 
-const installedDeps = runCommand(installDepsCommand)
-if (!installedDeps) {
-	process.exit(1)
+	const checkedOutput = runCommand(gitCheckoutCommand)
+
+	if (!checkedOutput) {
+		console.clear()
+		console.error('\x1b[31mFailed to clone into repo\x1b[0m')
+		process.exit(1)
+	}
+
+	console.log(`${MAGENTA}Installing dependencies...${RESET}`)
+
+	const installedDeps = runCommand(installDepsCommand)
+	if (!installedDeps) {
+		process.exit(1)
+	}
+	console.clear()
+	console.log(`${YELLOW}Done! Now do the following to start development:${RESET}`)
+	console.log(`${CYAN}cd ${repoName} && npm run dev${RESET}`)
+	console.log(`${MAGENTA}Dont forget to add .env back into .gitignore${RESET}`)
 }
-
-console.log('Done! Now do the following to start:')
-console.log(`cd ${repoName} && npm run dev`)
--- db/index.ts
+++ db/index.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose'
 
-const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/employee-manager'
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/database'
 
 mongoose
 	.connect(MONGO_URI)
 	.then(() => {
 		console.log(`Connected to Database.`)
 	})
-	.catch((err: {}) => {
-		console.error('Error connecting to Database. ', err)
+	.catch((error: {}) => {
+		console.error('Error connecting to Database. ', error)
 	})
